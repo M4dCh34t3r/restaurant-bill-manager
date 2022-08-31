94ca6ea6d17ff5b8da2bb5d9dee0bbcb72797e15
Pedro Andrade <pedroandrade20xx@gmail.com>
2022-08-31 16:20:38 -0300

    feat: add 'fetch data' from the API in the 'Item' page

--- src/pages/items/Items.tsx
+++ src/pages/items/Items.tsx
@@ -1,27 +1,30 @@
 import ItemSection from "../../components/item-section/ItemSection";
+import { BillItem } from "../../data/@types/billItem";
+import { useEffect, useState } from 'react';
 
 export function Items() {
+    const [billItems, setBillItems] = useState(Array<BillItem>);
+    const url = 'http://localhost:5000/bill-items';
+
+    useEffect(() =>  {
+      const fetchData = async () => {
+        try {
+          let response = await fetch(url);
+  
+          if (response.status === 200) {
+            console.log("Data successfully fetched from ", url);
+            let data = await response.json();
+            setBillItems(data);
+            return;
+          }
+        } catch(e: any) {
+          console.log(e);
+        }
+      }
+      fetchData();
+    });
+
     return(
-        <ItemSection 
-            billItems={ [
-                {
-                    id:0,
-                    name:"item 0",
-                    image:"/items/item0.png",
-                    price:100,
-                },
-                {
-                    id:1,
-                    name:"item 1",
-                    image:"/items/item1.png",
-                    price:50,
-                },
-                {
-                    id:2,
-                    name:"item 2",
-                    image:"/items/item2.png",
-                    price:25,
-                }
-            ] }/>
+        <ItemSection billItems={ billItems }/>
     );
 }
